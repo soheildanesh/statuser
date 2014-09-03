6bb1ebcec775d61b38ce544e66307fbac0f760a1
soheildanesh <soheild@soheils-mini.attlocal.net>
2014-09-03 17:44:47 -0500

    task list array in display (html) but hash in the controller and back

--- app/controllers/tasklist_generator_controller.rb
+++ app/controllers/tasklist_generator_controller.rb
@@ -10,10 +10,10 @@ class TasklistGeneratorController < ApplicationController
         uploaded_io = params['spreadSheet']['taskFile']
         @s = open_spreadsheet(uploaded_io)
         
-        @tasks = Array.new
+        @tasks = Hash.new
         for r in @s.first_row.to_i .. @s.last_row.to_i
             if not @s.row(r)[0].to_s.empty?
-                @tasks << { 'task number' => r.to_s,  'task' => @s.row(r)[0], 'unit' => @s.row(r)[1] ,'quantity' => @s.row(r)[2], 'value percentage' => @s.row(r)[3]  } 
+                @tasks[r.to_s] = { 'task number' => r.to_s,  'task' => @s.row(r)[0], 'unit' => @s.row(r)[1] ,'quantity' => @s.row(r)[2], 'value percentage' => @s.row(r)[3]  } 
             end    
         end
         
@@ -32,20 +32,21 @@ class TasklistGeneratorController < ApplicationController
         @project = $project_collection.find({:_id => BSON::ObjectId(params['projectId'].to_s) } ).to_a[0]
         @tasks = @project['tasks']
         if params.has_key? 'sort by start time' and params['sort by start time'] == 'true'
+            @tasks = @tasks.sort_by{ |taskNum, task| task["start date"]}
             
-            tasksWithNoStartDate = Array.new
-            tasksWithStartDate = Array.new
-            for task in @tasks
-                if not task.has_key? 'start date'
-                    tasksWithNoStartDate << task
-                else
-                    tasksWithStartDate << task
-                end 
-            end
-            tasksWithStartDate.sort!{|x,y| 
-                Time.new(x["start date"].to_s.split('-')[0], x["start date"].to_s.split('-')[1], x["start date"].to_s.split('-')[2].split()[0]) <=> Time.new(y["start date"].to_s.split('-')[0], y["start date"].to_s.split('-')[1], y["start date"].to_s.split('-')[2].split()[0])}
+            #tasksWithNoStartDate = Array.new
+            #tasksWithStartDate = Array.new
+            #@tasks.each do |taskNum, task|
+            #    if not task.has_key? 'start date'
+            #        tasksWithNoStartDate << task
+            #    else
+            #        tasksWithStartDate << task
+            #    end 
+            #end
+            #tasksWithStartDate.sort!{|x,y| 
+            #    Time.new(x["start date"].to_s.split('-')[0], x["start date"].to_s.split('-')[1], x["start date"].to_s.split('-')[2].split()[0]) <=> Time.new(y["start date"].to_s.split('-')[0], y["start date"].to_s.split('-')[1], y["start date"].to_s.split('-')[2].split()[0])}
                 
-            @tasks = tasksWithStartDate.concat tasksWithNoStartDate
+            #@tasks = tasksWithStartDate.concat tasksWithNoStartDate
             
         elsif params.has_key? 'sort by due date' and params['sort by due date'] == 'true'
             
@@ -80,33 +81,33 @@ class TasklistGeneratorController < ApplicationController
         action = params['actionSelect']
         
         if not selectedTasks.nil?
-            selectedTasks.each do |taskNum, theNumberOne|
+            selectedTasks.each do |taskNum, theNumberOne_dummy|
             
                 if action == 'set due date' and params.has_key? 'due date'
                     duedateHash = params['due date']
-                    @tasks[taskNum.to_i]['dueDate'] = Time.new(duedateHash["date(1i)"], duedateHash["date(2i)"], duedateHash["date(3i)"])
-                    @tasks[taskNum.to_i]['dueDate set by'] = get_current_user['_id']
-                    @tasks[taskNum.to_i]['dueDate set at'] = Time.now
+                    @tasks[taskNum]['dueDate'] = Time.new(duedateHash["date(1i)"], duedateHash["date(2i)"], duedateHash["date(3i)"])
+                    @tasks[taskNum]['dueDate set by'] = get_current_user['_id']
+                    @tasks[taskNum]['dueDate set at'] = Time.now
                 end
                 
                 if action == 'set start date' and params.has_key? 'start date'
                     startDateHash = params['start date']
-                    @tasks[taskNum.to_i]['start date'] = Time.new(startDateHash["date(1i)"], startDateHash["date(2i)"], startDateHash["date(3i)"])
-                    @tasks[taskNum.to_i]['start date set by'] = get_current_user['_id']
-                    @tasks[taskNum.to_i]['start date set at'] = Time.now
+                    @tasks[taskNum]['start date'] = Time.new(startDateHash["date(1i)"], startDateHash["date(2i)"], startDateHash["date(3i)"])
+                    @tasks[taskNum]['start date set by'] = get_current_user['_id']
+                    @tasks[taskNum]['start date set at'] = Time.now
                     
                 end
             
                 if action == 'mark done'
-                    @tasks[taskNum.to_i]['isDone'] = true
-                    @tasks[taskNum.to_i]['makred done by'] = get_current_user['_id']
-                    @tasks[taskNum.to_i]['marked done at'] = Time.now
+                    @tasks[taskNum]['isDone'] = true
+                    @tasks[taskNum]['makred done by'] = get_current_user['_id']
+                    @tasks[taskNum]['marked done at'] = Time.now
                 end
                 
                  if action == 'undo mark done'
-                    @tasks[taskNum.to_i]['isDone'] = false
-                    @tasks[taskNum.to_i]['undo mark done by'] = get_current_user['_id']
-                    @tasks[taskNum.to_i]['undo mark done at'] = Time.now
+                    @tasks[taskNum]['isDone'] = false
+                    @tasks[taskNum]['undo mark done by'] = get_current_user['_id']
+                    @tasks[taskNum]['undo mark done at'] = Time.now
                  end
             end
         end
@@ -135,31 +136,40 @@ class TasklistGeneratorController < ApplicationController
                     file.write(taskfile.read)
                 end 
                 
-                if not @tasks[taskNum.to_i].has_key? 'files'
-                    @tasks[taskNum.to_i]['files'] = Hash.new
+                if not @tasks[taskNum].has_key? 'files'
+                    @tasks[taskNum]['files'] = Hash.new
                 end
-                numExistingTaskFiles = @tasks[taskNum.to_i]['files'].size
-                @tasks[taskNum.to_i]['files'][numExistingTaskFiles.to_s] = taskfile.original_filename
+                numExistingTaskFiles = @tasks[taskNum]['files'].size
+                @tasks[taskNum]['files'][numExistingTaskFiles.to_s] = taskfile.original_filename
             end
                 
         end
+
         
         taskComments = params['taskComments']
         if not taskComments.nil?
-           
            taskComments.each do |taskNum, comment|
-               @tasks[taskNum.to_i]['comment'] = comment
+               
+                       
+               puts("tasNum = #{taskNum.to_i}")
+               puts("tasks = #{@tasks}")
+               
+               
+               @tasks[taskNum]['comment'] = comment
            end 
         end
         
         quantity_done = params['quantity_done']        
         if not quantity_done.nil?
             quantity_done.each do |taskNum, qdone|
-                @tasks[taskNum.to_i]['quantity_done'] = qdone
+                @tasks[taskNum]['quantity_done'] = qdone
             end 
         end
         
         $project_collection.save(@project)
+        @tasksArray = @tasks.sort
+        
+        puts("tasksArray = #{@tasksArray}")
         
     end
     
@@ -167,6 +177,7 @@ class TasklistGeneratorController < ApplicationController
         projectId = params["id"]
         @project = $project_collection.find({:_id => BSON::ObjectId(projectId.to_s) } ).to_a[0]
         @tasks = @project['tasks']
+        #@tasksArray = @tasks.sort
         
         render 'update'
     end
@@ -174,7 +185,7 @@ class TasklistGeneratorController < ApplicationController
     def showTaskFiles
         projectId = params["id"]
         @project = $project_collection.find({:_id => BSON::ObjectId(projectId.to_s) } ).to_a[0]
-        @taskNum = params['taskNum'].to_i
+        @taskNum = params['taskNum']
         @task = @project['tasks'][@taskNum]
         
     end
